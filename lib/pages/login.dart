59aac305ae87016370753a95c39489c0940995a3
amSreejith <csreejithofficial.com>
2024-09-21 11:20:07 +0530

    80% completed

--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -1,36 +1,82 @@
 import 'package:flutter/material.dart';
 
-class Login extends StatefulWidget {
+class Login extends StatelessWidget {
   const Login({super.key});
 
-  @override
-  State<Login> createState() => _LoginState();
-}
-
-class _LoginState extends State<Login> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.grey[300],
       body: SafeArea(
-        child: Column(
-          children: [
-            // Logo image
-            Center(child: Image.asset('assets/images/login.png', height: 100)),
-          const SizedBox(height: 25),
-            //center text
-            Text(
-              'HELLO AGAIN!',
-              style: TextStyle(color: Colors.grey[900], fontSize: 35),
-            ),
-             const Text('Welcome back, you\'ve been missed!'),
-             
-            //email pass field
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              //image
+              Image.asset('assets/images/login.png', height: 100),
+              const SizedBox(height: 20),
 
-            //signin btn
+              //hello again text
+              const Text('Hello Again! ',
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 30)),
+              const SizedBox(height: 10),
+              const Text('Welcome back, you\'ve missed! ',
+                  style: TextStyle(fontSize: 20)),
+              const SizedBox(height: 20),
+              //email textfield
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 25.0),
+                child: Container(
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(15),
+                        color: Colors.grey[100],
+                        border: Border.all(color: Colors.white)),
+                    padding: const EdgeInsets.all(5),
+                    child: const Padding(
+                      padding: EdgeInsets.only(left: 10.0),
+                      child: TextField(
+                        decoration: InputDecoration(
+                            border: InputBorder.none, hintText: 'Email'),
+                      ),
+                    )),
+              ),
+              const SizedBox(height: 10),
+              //password textfied
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 25.0),
+                child: Container(
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(15),
+                        color: Colors.grey[100],
+                        border: Border.all(color: Colors.white)),
+                    padding: const EdgeInsets.all(5),
+                    child: const Padding(
+                      padding: EdgeInsets.only(left: 10.0),
+                      child: TextField(
+                        obscureText: true,
+                        decoration: InputDecoration(
+                            border: InputBorder.none, hintText: 'Password'),
+                      ),
+                    )),
+              ),
+              const SizedBox(height: 10),
 
-            //register
-          ],
+              //sign in btn
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 25.0),
+                child: Container(
+                  padding: const EdgeInsets.all(20),
+                  width: double.infinity,
+                  decoration: BoxDecoration(
+                    color: Colors.deepPurple,
+                    borderRadius: BorderRadius.circular(15)
+                  ),
+                  child:  const Center(child: Text('Log In',style:TextStyle(color: Colors.white,fontSize: 19),)),
+                ),
+              )
+
+            ],
+          ),
         ),
       ),
     );
